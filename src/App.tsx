e0139f2b4c570897678bde2b3d0bfd8de3461977
Ricardo Rocha <cardrocharj@gmail.com>
2023-03-02 20:57:10 -0300

    configurando o ambiente de desenvolvimento

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import EstiloGlobal from './styles'
+import Teste from './Teste'
+
+function App() {
+  return (
+    <>
+      <EstiloGlobal />
+      <Teste />
+    </>
+  )
+}
+
+export default App
